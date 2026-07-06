eeb0d2a545d879f11d675ee4c4bc1ba1360d5e4d
Jonathan Charette <jacharette@icloud.com>
2026-07-06 11:48:21 -0400

    Fixed logic

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    console.log('DEBUG TEST')
     const button = document.getElementById('button');
 
     button.addEventListener('click', () => {
+        console.log('DEGUB TEST')
         if (button.classList.contains('inactive')) {
             button.classList.remove('inactive');
             button.classList.add('active');
